54cd1e7d66dc533d50dddf3cee355c36b59ebbf2
Stephantouchh <81231717+Stephantouchh@users.noreply.github.com>
2022-01-20 16:10:59 +0300

    update2

---
+++ EntityLayer/Concrete/Author.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class Author
+    {
+        [Key]
+        public int AuthorID { get; set; }
+        [StringLength(50)]
+        public string AuthorName { get; set; }
+        [StringLength(100)]
+        public string AuthorImage { get; set; }
+        [StringLength(250)]
+        public string AuthorAbout { get; set; }
+
+
+        public ICollection<Blog> Blogs { get; set; }
+    }
+}
